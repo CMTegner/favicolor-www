028209ab6ccb765cb9ca4c0f957174efdb75efeb
Christian Tegnér <christian.tegner@gmail.com>
2014-08-27 17:20:42 +0200

    Add example code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var on = require('component-delegate').bind;
 var favicolor = require('favicolor');
+var prism = require('prismjs');
 
 // Borrowed from https://github.com/Modernizr/Modernizr/blob/f526d97579d7ab42031bb2b852e8551d7b9f87e2/feature-detects/touchevents.js#L40
 if (('ontouchstart' in window) || (window.DocumentTouch && document instanceof DocumentTouch)) {
@@ -23,9 +24,14 @@ function selectColor(element) {
 var colorContainer = document.querySelector('[data-role=color-container]');
 var icon = document.querySelector('[rel=icon]');
 var fauxIcon = document.querySelector('[data-role=faux-icon]');
+var code = document.querySelectorAll('code');
 
 on(colorContainer, '[data-role=color]', 'click', function(event) {
     selectColor(event.target);
 });
+
+for (var i = 0; i < code.length; i++) {
+    prism.highlightElement(code[i]);
+}
 
 selectColor(colorContainer.children[4]);
